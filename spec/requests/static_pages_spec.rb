e588cca6f20f15ed7fe79fc138b0e0df9581265c
Raymond Maung <raymaung@gmail.com>
2012-07-21 19:43:17 +1000

    Add full_title to spec/utilities.rb and app/helpers/application_helper.rb

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,45 +2,44 @@ require 'spec_helper'
 
 describe "StaticPages" do
 
-	subject { page }
+  subject { page }
 
-	describe "Home page" do
+  describe "Home page" do
 
-		before {visit root_path }
+    before {visit root_path }
 
-		it { should have_selector('h1', text: 'Sample App')}
-		it { should have_selector('title',
-									text: "Ruby on Rails Tutorial Sample App")}
-		it { should_not have_selector('title', text: '| Home')}
-	end
+    it { should have_selector('h1', text: 'Sample App')}
+    it { should have_selector('title', text: full_title('Sample App'))}
+    it { should_not have_selector('title', text: '| Home')}
+  end
 
-	describe "Help page" do
+  describe "Help page" do
 
-		before { visit help_path }
+    before { visit help_path }
 
-		it { should have_selector('h1', :text => 'Help') }
-		it { should have_selector('title',
-				:text => "Ruby on Rails Tutorial Sample App | Help")}
-	end
+    it { should have_selector('h1', :text => 'Help') }
+    it { should have_selector('title',
+        :text => "Ruby on Rails Tutorial Sample App | Help")}
+  end
 
-	describe "About page" do
+  describe "About page" do
 
-		before { visit about_path }
+    before { visit about_path }
 
-		it { should have_selector('h1', :text => 'About Us') }
-		
-		it { should have_selector('title', 
-					:text => "Ruby on Rails Tutorial Sample App | About Us")}
-		
-	end
+    it { should have_selector('h1', :text => 'About Us') }
+    
+    it { should have_selector('title', 
+          :text => "Ruby on Rails Tutorial Sample App | About Us")}
+    
+  end
 
-	describe "Contact Page" do
+  describe "Contact Page" do
 
-		before {visit contact_path }
+    before {visit contact_path }
 
-		it { should have_selector('h1', text: 'Contact') }
+    it { should have_selector('h1', text: 'Contact') }
 
-		it { should have_selector('title',
-					text: "Ruby on Rails Tutorial Sample App | Contact") } 
-	end
+    it { should have_selector('title',
+          text: "Ruby on Rails Tutorial Sample App | Contact") } 
+  end
 end
