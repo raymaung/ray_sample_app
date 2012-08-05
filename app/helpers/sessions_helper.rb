ce77cd2a2b55ca4212491dbbbb1325ccbb8f155c
Raymond Maung <raymaung@gmail.com>
2012-08-05 12:22:12 +1000

    Hide Sign In link when the user is signed in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,6 +2,19 @@ module SessionsHelper
   def sign_in(user)
     # Sign the user in
     cookies.permanent[:remember_token] = { value: user.remember_token }
-
+    current_user = user
   end
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+
+  def current_user
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+  end
+
 end
