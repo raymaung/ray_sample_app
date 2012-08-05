343cf4b5bcae683c3ad9a6ddfee090d94b8341fa
Raymond Maung <raymaung@gmail.com>
2012-08-05 15:37:25 +1000

    Sign out the user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,19 +2,25 @@ module SessionsHelper
   def sign_in(user)
     # Sign the user in
     cookies.permanent[:remember_token] = { value: user.remember_token }
-    current_user = user
-  end
-
-  def current_user=(user)
-    @current_user = user
+    self.current_user = user
   end
 
   def signed_in?
     !current_user.nil?
   end
 
+  def current_user=(user)
+    @current_user = user
+  end
+
   def current_user
     @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+    @current_user
+  end
+
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
   end
 
 end
