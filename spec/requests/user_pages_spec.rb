2eef92a41f04c96963ced5c8492f52f935efb46d
Raymond Maung <raymaung@gmail.com>
2012-08-05 22:50:14 +1000

    Speed up user_pages_spec by running fake user creation just once for the tests. Lower the encryption key cost for the development environment

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,22 +8,29 @@ describe "UserPages" do
 
     let(:user) { FactoryGirl.create(:user) }
 
+    before(:all) do
+      30.times { FactoryGirl.create(:user) }
+    end
+
+    after(:all) { User.delete_all }
+
     before do
       sign_in user
-      30.times { FactoryGirl.create(:user) }
       visit users_path
     end
 
     it { should have_selector('title', text: 'All users')}
     it { should have_selector('h1', text: 'All users')}
 
-    it 'should list each user' do
-      User.paginate(page: 1).each do |user|
-        page.should have_selector('li>a', text: user.name)
+    describe "pagination" do
+      it 'should list each user' do
+        User.paginate(page: 1).each do |user|
+          page.should have_selector('li>a', text: user.name)
+        end
       end
-    end
 
-    it { should have_selector('div.pagination') }
+      it { should have_selector('div.pagination') }  
+    end
   end
 
   describe "signup page" do
